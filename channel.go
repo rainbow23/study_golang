d628e8a0c697c12b4407c3d7adbce3672c55f093
rainbow23 <info@rainbow23.jp>
2019-01-03 17:27:07 +0000

    map

--- channel.go
+++ channel.go
@@ -44,33 +44,65 @@ import (
 // }
 
 func main() {
-	n := make([]int, 3, 5)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
-	n = append(n, 0, 0)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
-	n = append(n, 1, 2, 3, 4, 5)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	// n := make([]int, 3, 5)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	// n = append(n, 0, 0)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	// n = append(n, 1, 2, 3, 4, 5)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
 
-	a := make([]int, 3)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), a)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
+	// a := make([]int, 3)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
+	// a = append(a, 1)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
+	// a = append(a, 1, 2, 3, 4, 5)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
 
-	b := make([]int, 0)
-	var c []int
-	fmt.Printf("len=%d cap=%d value=%v\n", len(b), cap(b), b)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c)
+	// b := make([]int, 0)
+	// var c []int
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(b), cap(b), b)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(b), cap(b), b)
 
-	c = make([]int, 5)
-	for i := 1; i < 6; i++ {
-		c = append(c, i)
-		fmt.Println(c)
+	// c = make([]int, 5)
+	// for i := 1; i < 6; i++ {
+	//     c = append(c, i)
+	//     fmt.Println(c)
+	// }
+	// fmt.Println(c)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c)
+
+	// c = make([]int, 0, 5)
+	// for i := 1; i < 6; i++ {
+	//     c = append(c, i)
+	//     fmt.Println(c)
+	// }
+	// fmt.Println(c)
+
+	m := map[string]int{"apple": 100, "banana": 200}
+	fmt.Println(m)
+	fmt.Println(m["apple"])
+	m["banana"] = 300
+	fmt.Println(m)
+	m["new"] = 500
+	fmt.Println(m)
+	fmt.Println(m["nothing"])
+
+	v, ok := m["apple"]
+	fmt.Println(v, ok)
+
+	v2, ok2 := m["nothing"]
+	fmt.Println(v2, ok2)
+
+	m2 := make(map[string]int)
+	m2["pc"] = 4000
+	fmt.Println(m2)
+
+	// var m3 map[string]int
+	// m3["pc"] = 5000
+	// fmt.Println(m3)
+
+	var s []int
+	if s == nil {
+		fmt.Println("Nil")
 	}
-	fmt.Println(c)
-
-	c = make([]int, 0, 5)
-	for i := 1; i < 6; i++ {
-		c = append(c, i)
-		fmt.Println(c)
-	}
-	fmt.Println(c)
 }
