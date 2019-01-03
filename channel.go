651f67f993e0c0441bef834dcf0f8d34c1da380a
rainbow23 <info@rainbow23.jp>
2019-01-03 04:23:38 +0000

    practice

--- channel.go
+++ channel.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
+	// "time"
 )
 
 // func listenChannel(myCh chan int) {
@@ -28,17 +28,42 @@ import (
 //     fmt.Printf("main exited now\n")
 // }
 
-func repeat(say string, count int) {
-	for i := 0; i < count; i++ {
-		time.Sleep(1 * time.Second)
-		fmt.Println(say, fmt.Sprintf("%d回目", i))
-	}
-}
+// func repeat(say string, count int) {
+//     for i := 0; i < count; i++ {
+//         time.Sleep(1 * time.Second)
+//         fmt.Println(say, fmt.Sprintf("%d回目", i))
+//     }
+// }
+
+// func main() {
+//     go repeat("平沢\t", 2)
+//     repeat("田井中\t", 6)
+//     go repeat("秋山\t", 5)
+//     go repeat("琴吹\t", 3)
+//     // repeat("田井中\t", 6)
+// }
 
 func main() {
-	go repeat("平沢\t", 2)
-	repeat("田井中\t", 6)
-	go repeat("秋山\t", 5)
-	go repeat("琴吹\t", 3)
-	// repeat("田井中\t", 6)
+	n := make([]int, 3, 5)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	n = append(n, 0, 0)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	n = append(n, 1, 2, 3, 4, 5)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+
+	a := make([]int, 3)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
+
+	b := make([]int, 0)
+	var c []int
+	fmt.Printf("len=%d cap=%d value=%v\n", len(b), cap(b), b)
+	fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c)
+
+	// c = make([]int, 5)
+	c = make([]int, 0, 5)
+	for i := 1; i < 6; i++ {
+		c = append(c, i)
+		fmt.Println(c)
+	}
+	fmt.Println(c)
 }
