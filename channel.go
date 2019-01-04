782bd7048ac4046b45545f1c3bdb5e339270c17d
rainbow23 <info@rainbow23.jp>
2019-01-04 14:30:44 +0000

    swith

--- channel.go
+++ channel.go
@@ -2,33 +2,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func getOsName() string {
+	return "aaaaa"
+}
+
 func main() {
-	l := []string{"python", "go", "java"}
-
-	for i := 0; i < len(l); i++ {
-		fmt.Println(i, l[i])
-	}
-	for i, v := range l {
-		fmt.Println(i, v)
-	}
-
-	for _, v := range l {
-		fmt.Println(v)
+	switch os := getOsName(); os {
+	case "mac":
+		fmt.Println("Mac!!")
+	case "windows":
+		fmt.Println("Windows!!")
+	default:
+		fmt.Println("Default!!", os)
 	}
 
-	m := map[string]int{"apple": 100, "banana": 200}
-
-	for k, v := range m {
-		fmt.Println(k, v)
-	}
-
-	for k := range m {
-		fmt.Println(k)
-	}
-
-	for _, v := range m {
-		fmt.Println(v)
+	t := time.Now()
+	fmt.Println(t.Hour())
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Morning")
+	case t.Hour() < 18:
+		fmt.Println("Afternoon")
 	}
 }
