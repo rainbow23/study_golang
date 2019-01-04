141d6e1d27677d762f7748d259560bd0ce4d4b6c
rainbow23 <info@rainbow23.jp>
2019-01-04 13:23:12 +0000

    for statement

--- channel.go
+++ channel.go
@@ -4,37 +4,22 @@ import (
 	"fmt"
 )
 
+func by2(num int) string {
+	if num%2 == 0 {
+		return "ok"
+	} else {
+		return "no"
+	}
+}
+
 func main() {
-	fmt.Println("slice")
-	var s1 []int
-	var s2 = []int{1, 2, 3, 4}
-	s3 := []int{1, 2, 3, 4}
+	result := by2(10)
 
-	fmt.Println(s1, len(s1), cap(s1))
-	fmt.Println(s2, len(s2), cap(s2))
-	fmt.Println(s3, len(s3), cap(s3))
-
-	s3s := s3[0:2]
-	fmt.Println(s3s, len(s3s), cap(s3s))
-
-	s3sc := s3[0:2:3]
-	fmt.Println(s3sc, len(s3sc), cap(s3sc))
-
-	fmt.Println("map")
-	var m1 map[int]string
-
-	var m2 = map[string]int{
-		"dora": 0,
-		"nobi": 1,
+	if result == "ok" {
+		fmt.Println("great")
 	}
 
-	m3 := map[string]int{
-		"shizu": 0,
-		"jai":   1,
-		"sune":  2,
+	if result2 := by2(10); result2 == "ok" {
+		fmt.Println("great 2")
 	}
-
-	fmt.Println(m1, len(m1))
-	fmt.Println(m2, len(m2))
-	fmt.Println(m3, len(m3))
 }
