8ba4850309cc703078a5b03298f92424b882d64e
rainbow23 <info@rainbow23.jp>
2019-01-04 13:58:42 +0000

    for

--- channel.go
+++ channel.go
@@ -5,24 +5,30 @@ import (
 )
 
 func main() {
-	for i := 0; i < 10; i++ {
-		if i == 3 {
-			fmt.Println("continue")
-			continue
-		}
-		if i == 4 {
-			fmt.Println("continue2")
-		}
-		if i > 5 {
-			fmt.Println("break")
-			break
-		}
-		fmt.Println(i)
+	l := []string{"python", "go", "java"}
+
+	for i := 0; i < len(l); i++ {
+		fmt.Println(i, l[i])
+	}
+	for i, v := range l {
+		fmt.Println(i, v)
 	}
 
-	sum := 1
-	for sum < 10 {
-		sum += sum
-		fmt.Println(sum)
+	for _, v := range l {
+		fmt.Println(v)
+	}
+
+	m := map[string]int{"apple": 100, "banana": 200}
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	for k := range m {
+		fmt.Println(k)
+	}
+
+	for _, v := range m {
+		fmt.Println(v)
 	}
 }
