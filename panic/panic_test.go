1835bc21b9e4f02b46cdca5d48e5373dd676810d
rainbow23 <info@rainbow23.jp>
2019-01-27 07:25:35 +0000

    interfaceまで追加

---
+++ panic/panic_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func thirdPartyConnectDB() {
+//     panic("Unable to connect database!")
+// }
+
+// func save() {
+//     thirdPartyConnectDB()
+// }
+
+func main() {
+	// save()
+	fmt.Println("OK?")
+
+}
